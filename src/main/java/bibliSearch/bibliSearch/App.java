6c835ad1750484b113af4105793347124e549ee7
axelzzz <axelzavier@gmail.com>
2020-02-24 00:02:27 +0100

    initial commit

---
+++ src/main/java/bibliSearch/bibliSearch/App.java
@@ -0,0 +1,13 @@
+package bibliSearch.bibliSearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
